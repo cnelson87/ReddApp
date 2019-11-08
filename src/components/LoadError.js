480881e5d09d82164db49867182dde89cefad470
Chris Nelson <chris.nelson@wearepop.com>
2019-11-08 11:41:19 -0800

    load error message

--- src/components/LoadError.js
+++ src/components/LoadError.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../styles/components/load-error.scss';
 
 function LoadError(props) {
 	const message = 'ERROR';
 
 	return (
-		<div className="error">
-			<p>{message}</p>
+		<div className="load-error">
+			<p className="load-error--message">{message}</p>
 			<p><Link to="/">Go to default view</Link></p>
 		</div>
 	);
